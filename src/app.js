9dce2c669163f2c078065457e1894ca6e5838937
Daron Short <daron.short@hotmail.com>
2018-01-05 20:47:27 -0600

    Built basic app

--- src/app.js
+++ src/app.js
@@ -1,37 +1,51 @@
 console.log("App.js is running!");
 
 const app = {
-  title: 'Indecision App',
-  subtitle: 'Make a decision'
-} 
+  title: "Indecision App",
+  subtitle: "Make a decision",
+  options: []
+};
 
-let template = (
-  <div>
-    <h1>{app.title}</h1>
-    <p>{app.subtitle}</p>
-    <ol>
-      <li>1. List 1</li>
-      <li>2. List 2</li>
-    </ol>
-  </div>
-);
+const onFormSubmit = event => {
+  event.preventDefault();
+  const option = event.target.elements.option.value;
+  if (option) {
+    app.options.push(option);
+    event.target.elements.option.value = "";
+  }
+  render();
+};
 
+const onRemoveAll = () => {
+  app.options = [];
+  render();
+};
 
+const onMakeChoice = () => {
+  const randomNum = Math.floor(Math.random() * app.options.lenth);
+  let option = app.options[randonNum];
+};
 
-var user = {
-  name: 'daron',
-  age: 29,
-  location: 'somewhere'
-}
-
-const templateTwo = (
-  <div>
-    <h1>{user.name}</h1>
-    <p>Age: {user.age}</p>
-    <p>Location: {user.location}</p>
-  </div>
-)
-
+const render = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? "Here are your options" : "No Options"}</p>
+      <button disabled={app.options.length == 0} onClick={onMakeChoice}> What Should I do </button>
+      <button onClick={onRemoveAll}>Remove All</button>
+      <ol>
+        {app.options.map(option => {
+          return <li key={option}>{option}</li>;
+        })}
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+};
 
 const appRoot = document.getElementById("app");
 
